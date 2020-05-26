210ecb40852d8cc5a98b2b4bfa4bf758c6090518
Alexrodriguez2090 <54908770+Alexrodriguez2090@users.noreply.github.com>
2020-05-25 22:00:34 -0500

    Update test_sample.py

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -4,10 +4,8 @@ import bot
 import random
 import aiohttp
 
-name = "hi"
 class TestName(object):
     def test_get_name(self):
         name = asyncio.get_event_loop().run_until_complete(bot.my_bot.getName(1))
         print(name)
         assert name == "Bulbasaur"
-print(name)
